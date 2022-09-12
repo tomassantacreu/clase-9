bef0006ab0dbbcfe91593ac3967c563a7c406465
tomassantacreu <tomassantacreu12@gmail.com>
2022-09-11 23:13:05 -0300

    terminado

---
+++ JS/scripts.js
@@ -0,0 +1,8 @@
+alert("Bienvenido");
+
+var pais = "Argentina";
+var ciudad = "San Nicolas de los Arroyos";
+var pais_y_ciudad = " Usted esta visitando esta pagina desde:" + " " + ciudad + "," + " " + pais;
+
+console.log(pais, ciudad);
+alert(pais_y_ciudad);
